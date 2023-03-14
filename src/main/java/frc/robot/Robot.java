5f7e3750148839ceba6c378bb5bc2419aa561fef
Gatlin Farrington <72674932+gatlinfarrington@users.noreply.github.com>
2023-03-14 16:31:16 -0700

    smartDashboard putData is not working.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -91,6 +91,7 @@ public class Robot extends TimedRobot {
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {
+    SmartDashboard.putData("Auto Choices", m_chooser);
     RobotContainer.m_Arm.zeroEncoder();
   }
 
