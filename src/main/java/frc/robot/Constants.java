38d71b80caf32f20ce288d7aa1d827730b6c9eba
Team-Paradise-1165 <107153539+Team-Paradise-1165@users.noreply.github.com>
2023-03-15 18:30:36 -0700

    Middle and top tuning #1

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,13 +21,13 @@ public final class Constants {
   public static class EncoderConstants{
     public static int STARTING_COUNT = 0;
 
-    public static final int FRONT_TOP_COUNT = 12000;
-    public static final int FRONT_MIDDLE_COUNT = 13500;
+    public static final int FRONT_TOP_COUNT = 13500;
+    public static final int FRONT_MIDDLE_COUNT = 17000;
     public static final int FRONT_BOTTOM_COUNT = 25000; //adjust for zeroing
     //arm rotation encoder counts
     public static final int BACK_BOTTOM_COUNT = -25062; //adjust for zeroing
-    public static final int BACK_MIDDLE_COUNT = -13208;
-    public static final int BACK_TOP_COUNT = -12000;
+    public static final int BACK_MIDDLE_COUNT = -17000;
+    public static final int BACK_TOP_COUNT = -13208;
     //telescoping arm encoder counts
     public static final int ARM_EXTENDED_COUNT = 0;
     public static final int ARM_UNEXTENDED_COUNT = 0;
